38fa452ae7b8cd88a443d17c6ae81a61f8702ade
Mojirade18 <mojiradeoluranti@gmail.com>
2023-11-04 07:00:58 -0700

    lets go

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    return a ** b
+
+# Test the function with the provided code in 11-main.py
+if __name__ == "__main__":
+    print(pow(2, 2))
+    print(pow(98, 2))
+    print(pow(98, 0))
+    print(pow(100, -2))
+    print(pow(-4, 5))
+
