d39357ae480634b8fb05f3e0c39e935ef425f8d4
Mojirade18 <mojiradeoluranti@gmail.com>
2023-11-04 06:20:58 -0700

    i like it this way

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 # Author: MOJIRADE OLURANTI
 
+output = ""
 for letter in range(97, 123):
     if chr(letter) not in 'qe':
-        print(chr(letter), end="")
+        output += chr(letter)
+
+print(output, end="")
+print()  # Add a newline at the end to match the desired output
 
