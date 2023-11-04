f5e2976c468c1673e9849491ebcbd05f36ad1653
Mojirade18 <mojiradeoluranti@gmail.com>
2023-11-04 08:45:17 -0700

    i love it all

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+a = 10
+b = 5
+
+from calculator_1 import add, sub, mul, div
+
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
+
+print("{0} + {1} = {2}".format(a, b, result_add))
+print("{0} - {1} = {2}".format(a, b, result_sub))
+print("{0} * {1} = {2}".format(a, b, result_mul))
+print("{0} / {1} = {2}".format(a, b, result_div))
+
