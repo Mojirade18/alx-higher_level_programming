e651630cc693122fca2f7433ddd82c233f64f95f
Mojirade18 <mojiradeoluranti@gmail.com>
2023-11-04 06:49:31 -0700

    its okay to be okay

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-for first_digit in range(10):
+for first_digit in range(0, 10):
     for second_digit in range(first_digit + 1, 10):
-        if first_digit != second_digit:
-            if first_digit < 9:
-                print("{:d}{:d}, ".format(first_digit, second_digit), end="")
+        if first_digit == 8 and second_digit: == 9
+                print("{}{}".format(first_digit, second_digit))
             else:
-                print("{:d}{:d}".format(first_digit, second_digit))
+                print("{}{}".format(first_digit, second_digit), end=", ")
 
 print()  # Add a newline at the end to match the expected output
 
