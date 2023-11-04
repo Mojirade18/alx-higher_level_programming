078ceb3a78013eee352c93a7d4f912bf219ca41b
Mojirade18 <mojiradeoluranti@gmail.com>
2023-11-04 06:28:53 -0700

    i like it this way

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 for letter in range(97, 123):
-    if chr(letter) not in 'qe':
-        print(chr(letter), end='')  # This single print statement without .format()
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}". format(chr(letter)), end="")  # This single print statement without .format()
 
 print()  # Add a newline at the end to match the desired output
 
