9aabc4bd5d0334b2b99eef55fc1126fa4b2398d5
Mojirade18 <mojiradeoluranti@gmail.com>
2023-11-04 06:18:59 -0700

    i like it this way

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# Author - MOJIRADE OLURANTI
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}". format(chr(letter)), end="")
+# Author: MOJIRADE OLURANTI
 
+for letter in range(97, 123):
+    if chr(letter) not in 'qe':
+        print(chr(letter), end="")
 
