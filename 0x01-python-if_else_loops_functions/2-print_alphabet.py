d4c705696ef259cddfb668250c3f07f2f8e1dc3b
Mojirade18 <mojiradeoluranti@gmail.com>
2023-11-04 05:54:38 -0700

    i have tried to be good

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-for ascii_value in range(ord('a'), ord('z') + 1):
-    print(chr(ascii_value), end='')
+for char in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(char), end='')
 
-print()
+print()  # Add a newline at the end to match the desired output
+
