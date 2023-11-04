0eb8a124cc5a26684908573b238fd10179bc47b0
Mojirade18 <mojiradeoluranti@gmail.com>
2023-11-04 06:59:18 -0700

    lets go

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    return a + b
+
+# Test the function with the provided code in 10-main.py
+if __name__ == "__main__":
+    print(add(1, 2))
+    print(add(98, 0))
+    print(add(100, -2))
+
