2ae484012b12b7ef8994e5b4f3985ed74859e447
Mojirade18 <mojiradeoluranti@gmail.com>
2023-11-06 11:53:46 -0800

    i like it

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Extract the first two elements from each tuple (or use 0 if missing)
-    a1, a2 = tuple_a[:2] + (0, 0)
-    b1, b2 = tuple_b[:2] + (0, 0)
+    # Use index-based access to get the first and second elements of each tuple (or use 0 if missing)
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
 
     # Calculate the sum of the corresponding elements
     result = (a1 + b1, a2 + b2)
 
     return result
-
