f621438e30096cadd1cffd6e31224b77bc7d7c5e
Mojirade18 <mojiradeoluranti@gmail.com>
2023-11-12 07:34:28 -0800

    i love it

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """
+    Returns a new dictionary with all values multiplied by 2.
+
+    Args:
+    - a_dictionary: Dictionary.
+
+    Returns:
+    - New dictionary with values multiplied by 2.
+    """
+    # Create a new dictionary with values multiplied by 2
+    new_dict = {key: value * 2 for key, value in a_dictionary.items()}
+
+    return new_dict
+
+# Example usage:
+if __name__ == "__main__":
+    a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+    new_dict = multiply_by_2(a_dictionary)
+    print_sorted_dictionary(a_dictionary)
+    print("--")
+    print_sorted_dictionary(new_dict)
