78184c118280cc75e894d97c4cadf6db687139c7
Mojirade18 <mojiradeoluranti@gmail.com>
2023-11-12 07:36:22 -0800

    i love it

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """
+    Returns a key with the biggest integer value.
+
+    Args:
+    - a_dictionary: Dictionary.
+
+    Returns:
+    - Key with the biggest integer value.
+    """
+    if a_dictionary:
+        # Find the key with the maximum value
+        best_key = max(a_dictionary, key=a_dictionary.get)
+        return best_key
+    else:
+        return None
+
+# Example usage:
+if __name__ == "__main__":
+    a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+    best_key = best_score(a_dictionary)
+    print("Best score: {}".format(best_key))
+
+    best_key = best_score(None)
+    print("Best score: {}".format(best_key))
